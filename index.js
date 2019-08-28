99156fd0be4e435e8157e1b556bfe0c372323982
Fran Borg <francesca.bte@outlook.com>
2019-08-28 10:14:11 +0200

    Add files for solution

---
+++ index.js
@@ -0,0 +1,17 @@
+const books = [
+    {
+        title: 'The Design of EveryDay Things',
+        author: 'Don Norman',
+        alreadyRead: false
+    }, {
+        title: 'The Most Human Human',
+        author: 'Brian Christian',
+        alreadyRead: true
+    }
+];
+
+let title = document.querySelector('h1');
+books.forEach(book => {
+    let title = document.querySelector('h1');
+    title.document.appendChild(`<p></p>`);
+})
