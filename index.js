67cd2e98cbb8b3532c8b79f49df1e022fd6f83e7
Simon <simonsch.tz@gmail.com>
2020-01-27 11:12:24 +0100

    Write Pseudo code

--- index.js
+++ index.js
@@ -27,3 +27,32 @@ const books = [
     img: "https://eloquentjavascript.net/img/cover.jpg"
   }
 ];
+
+/**
+ * traverse through array
+ *  -> forEach
+ *      -> create li
+ *      -> append img
+ *      -> crate h2
+ *      -> append title to h2
+ *      -> create div .author
+ *      -> append Author[1], Author[0]
+ *      -> create div .status
+ *      -> append alreadyRead
+ *  li
+ *      append img
+ *    h2
+ *        append title
+ *    /h2
+ *    div
+ *        append author
+ *        --- Family name, First name
+ *    /div
+ *    div
+ *        append alreadyRead
+ *    /div
+ *  /li
+ * 
+ */
+
+console.log(books[0].title);
